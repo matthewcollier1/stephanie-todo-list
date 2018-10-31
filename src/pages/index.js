8979149659e15306981a1d6654ede90286b17817
winstonfarsoul <matthewjcollier@gmail.com>
2018-10-31 11:14:31 -0400

    animated birds

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,21 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <div className="container">
+          <div class="bird-container bird-container--one">
+            <div class="bird bird--one" />
+          </div>
+
+          <div class="bird-container bird-container--two">
+            <div class="bird bird--two" />
+          </div>
+
+          <div class="bird-container bird-container--three">
+            <div class="bird bird--three" />
+          </div>
+
+          <div class="bird-container bird-container--four">
+            <div class="bird bird--four" />
+          </div>
           <TodoList />
         </div>
       </Layout>
